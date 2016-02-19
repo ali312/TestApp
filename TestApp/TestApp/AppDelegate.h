bce7f68bd2da5ba3a1b614384ad4bbbc5dbaa169
ali312 <ali312@yandex.ru>
2016-02-19 21:11:52 +0600

    Initial commit

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Антон Кузнецов on 19/02/16.
+//  Copyright © 2016 thelightprj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Антон Кузнецов on 19/02/16.
+//  Copyright © 2016 thelightprj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
